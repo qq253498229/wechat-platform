6d9564fce0485626d3e985a807b67d596cfaa722
大胃王 <253498229@qq.com>
2018-10-26 17:46:44 +0800

    polish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,26 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
 import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './shared/exception/page-not-found/page-not-found.component';
 import {LayoutComponent} from './shared/layout/layout.component';
-import {TranslateModule} from '@ngx-translate/core';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {HeaderComponent} from './shared/header/header.component';
 import {FooterComponent} from './shared/footer/footer.component';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {IndexComponent} from './shared/index/index.component';
 
 registerLocaleData(zh);
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 
 const routes: Routes = [
   {
@@ -44,7 +51,15 @@ const routes: Routes = [
     FormsModule,
     HttpClientModule,
     NgZorroAntdModule,
-    TranslateModule.forRoot(),
+    TranslateModule.forRoot(
+      {
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      }
+    ),
     RouterModule.forRoot(routes),
   ],
   providers: [{provide: NZ_I18N, useValue: zh_CN}],
